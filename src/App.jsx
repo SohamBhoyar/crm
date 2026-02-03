6f6c0acfe3b2a782e2bd1fd35fbb77b885cd95ef
SohamBhoyar <sohambhoyar096@gmail.com>
2026-02-03 16:05:03 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,13 @@
-// src/App.jsx
-
-// import AppRouter from "./app/AppRouter";
-// import { AuthProvider } from "@/auth/authStore";
-
-/**
- * Root App
- *
- * IMPORTANT RULE:
- * - AppRouter must ALWAYS render
- * - AuthProvider must NOT block public pages
- */
-
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <AppRouter />
-//     </AuthProvider>
-//   );
-// }
-
-// export default App;
-
 import AppRouter from "./app/AppRouter";
 import { AuthProvider } from "@/auth/authStore";
 
 function App() {
   return (
-    <>
-      <div style={{ color: "blue", textAlign: "center" }}>
-        BEFORE AUTH
-      </div>
-
-      <AuthProvider>
-        <div style={{ color: "green", textAlign: "center" }}>
-          INSIDE AUTH
-        </div>
-
-        <AppRouter />
-      </AuthProvider>
-
-      <div style={{ color: "red", textAlign: "center" }}>
-        AFTER AUTH
-      </div>
-    </>
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      PORTAL TEST VISIBLE
+    </div>
   );
 }
 
 export default App;
+
