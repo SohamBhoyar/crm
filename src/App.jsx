00a6e4a4dc045bab14b9f54ec3e4f53779fedfdb
SohamBhoyar <sohambhoyar096@gmail.com>
2026-02-03 15:21:32 +0530

    app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,22 @@
 // src/App.jsx
 
-import App from "@/app/App";
+import AppRouter from "./app/AppRouter";
+import { AuthProvider } from "@/auth/authStore";
+
+/**
+ * Root App
+ *
+ * IMPORTANT RULE:
+ * - AppRouter must ALWAYS render
+ * - AuthProvider must NOT block public pages
+ */
+
+function App() {
+  return (
+    <AuthProvider>
+      <AppRouter />
+    </AuthProvider>
+  );
+}
 
 export default App;
